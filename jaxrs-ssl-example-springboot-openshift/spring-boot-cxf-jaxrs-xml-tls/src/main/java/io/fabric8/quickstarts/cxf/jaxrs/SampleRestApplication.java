2eb0ffa6f243ad718f1b2011a4cdfc191d868b12
chandrashekhar <chandrashekhar@localhost>
2020-05-02 20:20:19 +0530

    EnableAutoConfiguration

--- jaxrs-ssl-example-springboot-openshift/spring-boot-cxf-jaxrs-xml-tls/src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
+++ jaxrs-ssl-example-springboot-openshift/spring-boot-cxf-jaxrs-xml-tls/src/main/java/io/fabric8/quickstarts/cxf/jaxrs/SampleRestApplication.java
@@ -15,13 +15,12 @@
  */
 package io.fabric8.quickstarts.cxf.jaxrs;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 
+@EnableAutoConfiguration
 @SpringBootApplication
 @ImportResource({"classpath:endpoint.xml"})
 public class SampleRestApplication {
